e0fdf16b72dac55bde8b38cacbd85392389b01d4
Tertegen2004 <stegen498@gmail.com>
2026-06-28 20:36:08 +0300

    Initial Commit

---
+++ Infrastructure/Common/DepenancyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Application.Interfaces;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Common
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // 1. تسجيل الـ DbContext
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped(typeof(IRepository<>),typeof(Repository<>));
+
+            return services;
+        }
+    }
+}
