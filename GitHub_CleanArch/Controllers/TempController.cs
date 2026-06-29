705b7c1879f108326e15f8c59da8b148f51179fa
Tertegen2004 <stegen498@gmail.com>
2026-06-29 15:44:43 +0300

    Add Temp Classes To Folders

---
+++ GitHub_CleanArch/Controllers/TempController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GitHub_CleanArch.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TempController : ControllerBase
+    {
+    }
+}
